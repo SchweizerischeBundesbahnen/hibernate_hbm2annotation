2d5c790be0a1aec0c2cf4efe99f33a3ff98bbbc6
Marius Schär <marius.schaer2@sbb.ch>
2018-12-19 12:20:32 +0100

    #12 also recognise addClass, not only addResource

--- Hbm/RegistrationMigrator.php
+++ Hbm/RegistrationMigrator.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Change registration from hbm to annotation class. 
+ * Change registration from hbm to annotation class.
  * Search and replace in all $activatorFiles (defined in config.php)
  * for:
  *    .addResource("com.example/path/THE_CLASS_NAME.hbm.xml", classLoader
@@ -11,34 +11,44 @@
  * If your framework wraps hibernate or doesn't looks like ours,
  * you may have to change this class to match your architecture.
  **/
-class RegistrationMigrator {
+class RegistrationMigrator
+{
 
     private $activatorFiles = array();
 
-    function __construct(array $activatorFiles) {
+    function __construct(array $activatorFiles)
+    {
         foreach ($activatorFiles as $filePath) {
-            $this->activatorFiles[ $filePath ] = file($filePath);
+            $this->activatorFiles[$filePath] = file($filePath);
         }
     }
 
-    function __destruct() {
+    function __destruct()
+    {
         foreach ($this->activatorFiles as $filePath => $lines) {
             file_put_contents($filePath, implode('', $lines));
         }
     }
 
-    public function migrate(string $hbmFile): bool {
+    public function migrate(string $hbmFile): bool
+    {
         $name = basename($hbmFile, '.hbm.xml');
-            $regex = '/(\.addResource\()[^)]*' . preg_quote($name) . '\.hbm.xml\", classLoader\)/';
-            $subst  = '.addAnnotatedClass(' . $name. '.class)';
-            foreach ($this->activatorFiles as $filePath => $lines) {
-                foreach ($lines as $index => $line) {
-                    if (preg_match($regex, $line)) {
-                        $this->activatorFiles[$filePath][$index] =
-                            preg_replace($regex, $subst, $line);
-                        return true;
-                    }
+
+        // Hibernate can be told about a class in two ways, addResource or addClass
+        $resourceRegex = '(\.addResource\()[^)]*' . preg_quote($name) . '\.hbm\.xml\", classLoader\)';
+        $classRegex = '(\.addClass\()[^)]*' . preg_quote($name) . '\.class\)';
+        $regex = "/($resourceRegex|$classRegex)/";
+
+        // both can be replaced with addAnnotatedClass when using annotations
+        $subst = '.addAnnotatedClass(' . $name . '.class)';
+        foreach ($this->activatorFiles as $filePath => $lines) {
+            foreach ($lines as $index => $line) {
+                if (preg_match($regex, $line)) {
+                    $this->activatorFiles[$filePath][$index] =
+                        preg_replace($regex, $subst, $line);
+                    return true;
                 }
+            }
         }
         return false;
     }
